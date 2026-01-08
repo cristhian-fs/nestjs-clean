da02c3ef0877585dad777b14b6040dbdfadbb3b7
cristhian <cristhianfernandolp@gmail.com>
2026-01-08 09:44:50 -0300

    feat: create prisma service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({ url: process.env.DATABASE_URL });
+    super({
+      adapter,
+      log: ['warn', 'error'],
+    });
+  }
+
+  onModuleInit() {
+    return this.$connect();
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect();
+  }
+}
