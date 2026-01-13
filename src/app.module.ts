885d4934902541d5d0154bb76a860fe5156595ba
cristhian <cristhianfernandolp@gmail.com>
2026-01-12 21:44:16 -0300

    feat(auth): add authenticate controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,17 @@ import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { envSchema } from './env';
 import { AuthModule } from './auth/app.module';
+import { AuthenticateController } from './controllers/authenticate.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    validate: env => envSchema.parse(env),
-    isGlobal: true,
-  }),
-    AuthModule
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
   ],
-  controllers: [CreateAccountController],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
