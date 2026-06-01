bf989dccc0f8ef5575bca016c0491ed14f44de33
cris <cristhianfernandolp@gmail.com>
2026-06-01 14:45:59 -0300

    feat: implement register student and authenticate student tests

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -1,7 +1,7 @@
 import { Entity } from '@/core/entities/entity.js';
 import type { UniqueEntityID } from '@/core/entities/unique-entity-id.js';
 
-interface StudentProps {
+export interface StudentProps {
   name: string;
   email: string;
   password: string;
