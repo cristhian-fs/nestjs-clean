e065ce2e22b7e37eba0c1030d2d1c3bbeeefa500
cris <cristhianfernandolp@gmail.com>
2026-06-05 11:31:02 -0300

    feat: adds edit-question controller and tests

--- src/domain/forum/application/use-cases/edit-question.ts
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -1,12 +1,13 @@
-import { type Either, right, left } from "@/core/either";
-import type { Question } from "../../enterprise/entities/question";
-import type { QuestionsRepository } from "../repositories/questions-repository";
-import { ResourceNotFoundError } from "./errors/resource-not-found-error";
-import { NotAllowedError } from "./errors/not-allowed-error";
-import { QuestionAttachmentsRepository } from "../repositories/question-attachments-repository";
-import { QuestionAttachmentList } from "../../enterprise/entities/question-attachment-list";
-import { QuestionAttachment } from "../../enterprise/entities/question-attachment";
-import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { type Either, right, left } from '@/core/either';
+import type { Question } from '../../enterprise/entities/question';
+import { QuestionsRepository } from '../repositories/questions-repository';
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
+import { NotAllowedError } from './errors/not-allowed-error';
+import { QuestionAttachmentsRepository } from '../repositories/question-attachments-repository';
+import { QuestionAttachmentList } from '../../enterprise/entities/question-attachment-list';
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { Injectable } from '@nestjs/common';
 
 interface EditQuestionUseCaseRequest {
   authorId: string;
@@ -23,6 +24,7 @@ type EditQuestionUseCaseResponse = Either<
   }
 >;
 
+@Injectable()
 export class EditQuestionUseCase {
   constructor(
     private questionsRepository: QuestionsRepository,
