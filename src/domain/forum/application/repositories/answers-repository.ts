666b69892391f9ab7f7bb22dfb1f0cd57ceb1397
cris <cristhianfernandolp@gmail.com>
2026-06-22 14:23:21 -0300

    feat(domain): add enriched query methods to QuestionsRepository and AnswersRepository contracts

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,5 +1,6 @@
 import { PaginationProps } from '@/core/repositories/pagination-props.js';
 import type { Answer } from '../../enterprise/entities/answer.js';
+import { AnswerWithAuthor } from '../../enterprise/entities/value-objects/answer-with-author.js';
 
 export abstract class AnswersRepository {
   abstract create(answer: Answer): Promise<void>;
@@ -7,6 +8,10 @@ export abstract class AnswersRepository {
     questionId: string,
     props: PaginationProps,
   ): Promise<Answer[]>;
+  abstract findManyByQuestionIdWithAuthor(
+    questionId: string,
+    props: PaginationProps,
+  ): Promise<AnswerWithAuthor[]>;
   abstract findById(id: string): Promise<Answer | null>;
   abstract save(question: Answer): Promise<void>;
   abstract delete(question: Answer): Promise<void>;
--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,6 +1,7 @@
 import type { PaginationProps } from '@/core/repositories/pagination-props';
 import type { Question } from '../../enterprise/entities/question';
 import { QuestionDetails } from '../../enterprise/entities/value-objects/question-details';
+import { QuestionSummary } from '../../enterprise/entities/value-objects/question-summary';
 
 export abstract class QuestionsRepository {
   abstract findBySlug(slug: string): Promise<Question | null>;
@@ -8,6 +9,7 @@ export abstract class QuestionsRepository {
   abstract create(question: Question): Promise<void>;
   abstract save(question: Question): Promise<void>;
   abstract findManyRecents(props: PaginationProps): Promise<Question[]>;
+	abstract findManyRecentsWithAuthor(props: PaginationProps): Promise<QuestionSummary[]>;
   abstract findById(id: string): Promise<Question | null>;
   abstract delete(question: Question): Promise<void>;
 }
